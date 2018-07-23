0ab707327870af42b2fc8458640f47ad04df08d1
saurav <sauravpd58@gmail.com>
2018-07-23 23:45:10 +0530

    added MyStack implementation

--- src/main/java/codeSamples/stack/MyStack.java
+++ src/main/java/codeSamples/stack/MyStack.java
@@ -2,5 +2,31 @@ package codeSamples.stack;
 
 public class MyStack 
 {
+	private int stackSize;
+	private int[] stackArr;
+	private int top;
 
+	public MyStack(int size) 
+	{
+		this.stackSize = size;
+		this.stackArr = new int[stackSize];
+		this.top = -1;
+	}
+
+	public void push(int entry) throws Exception 
+	{
+		if(this.isStackFull()){
+			throw new Exception("Stack is already full. Can not add element.");
+		}
+		System.out.println("Adding: "+entry);
+		this.stackArr[++top] = entry;
+	}
+
+	public boolean isStackEmpty() {
+		return (top == -1);
+	}
+
+	public boolean isStackFull() {
+		return (top == stackSize - 1);
+	}
 }
