7c319dcbe3f09deaf71a83de8349d4f5e3e89757
saurav <sauravpd58@gmail.com>
2018-07-29 01:10:35 +0530

    added student class

---
+++ src/main/java/codeSamples/linkedlist/Student.java
@@ -0,0 +1,27 @@
+package codeSamples.linkedlist;
+
+public class Student 
+{
+	int rollNo;
+	String name;
+	
+	public Student(int rollNo,String name) 
+	{
+		this.rollNo=rollNo;
+		this.name=name;
+	}
+	public int getRollNo()
+	{
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
