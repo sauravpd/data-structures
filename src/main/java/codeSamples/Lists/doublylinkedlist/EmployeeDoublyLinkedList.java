707163c3f8f370f1b14a5be4d747fca018ef9f3d
saurav <sauravpd58@gmail.com>
2018-10-28 22:23:13 +0530

    formated EmployeeDoublyLinkedList class

--- src/main/java/codeSamples/Lists/doublylinkedlist/EmployeeDoublyLinkedList.java
+++ src/main/java/codeSamples/Lists/doublylinkedlist/EmployeeDoublyLinkedList.java
@@ -5,98 +5,104 @@ package codeSamples.Lists.doublylinkedlist;
  * @author saurav
  *
  */
-public class EmployeeDoublyLinkedList {
+public class EmployeeDoublyLinkedList 
+{
+	private EmployeeNode head;
+	private EmployeeNode tail;
+	private int size;
 
-    private EmployeeNode head;
-    private EmployeeNode tail;
-    private int size;
+	public void addToFront(Employee employee)
+	{
+		EmployeeNode node = new EmployeeNode(employee);
 
-    public void addToFront(Employee employee) {
-        EmployeeNode node = new EmployeeNode(employee);
+		if (head == null) {
+			tail = node;
+		}
+		else {
+			head.setPrevious(node);
+			node.setNext(head);
+		}
 
-        if (head == null) {
-            tail = node;
-        }
-        else {
-            head.setPrevious(node);
-            node.setNext(head);
-        }
+		head = node;
+		size++;
+	}
 
-        head = node;
-        size++;
-    }
+	public void addToEnd(Employee employee) 
+	{
+		EmployeeNode node = new EmployeeNode(employee);
+		if (tail == null) {
+			head = node;
+		}
+		else {
+			tail.setNext(node);
+			node.setPrevious(tail);
+		}
 
-    public void addToEnd(Employee employee) {
-        EmployeeNode node = new EmployeeNode(employee);
-        if (tail == null) {
-            head = node;
-        }
-        else {
-            tail.setNext(node);
-            node.setPrevious(tail);
-        }
+		tail = node;
+		size++;
+	}
 
-        tail = node;
-        size++;
-    }
+	public EmployeeNode removeFromFront() 
+	{
+		if (isEmpty()) {
+			return null;
+		}
 
-    public EmployeeNode removeFromFront() {
-        if (isEmpty()) {
-            return null;
-        }
+		EmployeeNode removedNode = head;
 
-        EmployeeNode removedNode = head;
+		if (head.getNext() == null) {
+			tail = null;
+		}
+		else {
+			head.getNext().setPrevious(null);
+		}
 
-        if (head.getNext() == null) {
-            tail = null;
-        }
-        else {
-            head.getNext().setPrevious(null);
-        }
+		head = head.getNext();
+		size--;
+		removedNode.setNext(null);
+		return removedNode;
+	}
 
-        head = head.getNext();
-        size--;
-        removedNode.setNext(null);
-        return removedNode;
-    }
+	public EmployeeNode removeFromEnd() 
+	{
+		if (isEmpty()) {
+			return null;
+		}
 
-    public EmployeeNode removeFromEnd() {
-        if (isEmpty()) {
-            return null;
-        }
+		EmployeeNode removedNode = tail;
 
-        EmployeeNode removedNode = tail;
+		if (tail.getPrevious() == null) {
+			head = null;
+		}
+		else {
+			tail.getPrevious().setNext(null);
+		}
 
-        if (tail.getPrevious() == null) {
-            head = null;
-        }
-        else {
-            tail.getPrevious().setNext(null);
-        }
+		tail = tail.getPrevious();
+		size--;
+		removedNode.setPrevious(null);
+		return removedNode;
+	}
 
-        tail = tail.getPrevious();
-        size--;
-        removedNode.setPrevious(null);
-        return removedNode;
-    }
+	public int getSize() 
+	{
+		return size;
+	}
 
-    public int getSize() {
-        return size;
-    }
+	public boolean isEmpty() 
+	{
+		return head == null;
+	}
 
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    public void printList() {
-        EmployeeNode current = head;
-        System.out.print("HEAD -> ");
-        while (current != null) {
-            System.out.print(current);
-            System.out.print(" <=> ");
-            current = current.getNext();
-        }
-        System.out.println("null");
-    }
-
+	public void printList() 
+	{
+		EmployeeNode current = head;
+		System.out.print("HEAD -> ");
+		while (current != null) {
+			System.out.print(current);
+			System.out.print(" <=> ");
+			current = current.getNext();
+		}
+		System.out.println("null");
+	}
 }
