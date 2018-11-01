8a3a0a26f978d3c55549932a0e59702a90b5a7eb
saurav <sauravpd58@gmail.com>
2018-11-02 02:52:30 +0530

    added javadoc

--- src/main/java/codeSamples/Queues/arrayqueue/ArrayQueue.java
+++ src/main/java/codeSamples/Queues/arrayqueue/ArrayQueue.java
@@ -2,59 +2,66 @@ package codeSamples.Queues.arrayqueue;
 
 import java.util.NoSuchElementException;
 
-public class ArrayQueue {
+/**
+ * ArrayQueue
+ * @author saurav
+ *
+ */
+public class ArrayQueue 
+{
 
-    private Employee[] queue;
-    private int front;
-    private int back;
+	private Employee[] queue;
+	private int front;
+	private int back;
 
-    public ArrayQueue(int capacity) {
-        queue = new Employee[capacity];
-    }
+	public ArrayQueue(int capacity) 
+	{
+		queue = new Employee[capacity];
+	}
 
-    public void add(Employee employee) {
-        if (back == queue.length) {
-            Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
-            queue = newArray;
-        }
+	public void add(Employee employee)
+	{
+		if (back == queue.length) {
+			Employee[] newArray = new Employee[2 * queue.length];
+			System.arraycopy(queue, 0, newArray, 0, queue.length);
+			queue = newArray;
+		}
 
-        queue[back] = employee;
-        back++;
-    }
+		queue[back] = employee;
+		back++;
+	}
 
-    public Employee remove() {
-        if (size() == 0) {
-            throw new NoSuchElementException();
-        }
+	public Employee remove() {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
 
-        Employee employee = queue[front];
-        queue[front] = null;
-        front++;
-        if (size() == 0) {
-            front = 0;
-            back = 0;
-        }
+		Employee employee = queue[front];
+		queue[front] = null;
+		front++;
+		if (size() == 0) {
+			front = 0;
+			back = 0;
+		}
 
-        return employee;
-    }
+		return employee;
+	}
 
-    public Employee peek() {
-        if (size() == 0) {
-            throw new NoSuchElementException();
-        }
+	public Employee peek() {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
 
-        return queue[front];
-    }
+		return queue[front];
+	}
 
-    public int size() {
-        return back - front;
-    }
+	public int size() {
+		return back - front;
+	}
 
-    public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
-        }
-    }
-
+	public void printQueue() {
+		for (int i = front; i < back; i++) {
+			System.out.println(queue[i]);
+		}
+	}
 }
