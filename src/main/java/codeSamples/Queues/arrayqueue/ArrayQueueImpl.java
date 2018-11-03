9a4d6926da95586d8326b19d656ed1cb2a07f94e
saurav <sauravpd58@gmail.com>
2018-11-03 23:24:33 +0530

    added ArrayQueueImpl class

---
+++ src/main/java/codeSamples/Queues/arrayqueue/ArrayQueueImpl.java
@@ -0,0 +1,30 @@
+package codeSamples.Queues.arrayqueue;
+
+public class ArrayQueueImpl 
+{
+	public static void main(String[] args) 
+	{
+		Employee janeJones = new Employee("Jane", "Jones", 123);
+		Employee johnDoe = new Employee("John", "Doe", 4567);
+		Employee marySmith = new Employee("Mary", "Smith", 22);
+		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+		Employee billEnd = new Employee("Bill", "End", 78);
+
+		ArrayQueue queue = new ArrayQueue(10);
+		queue.add(janeJones);
+		queue.add(johnDoe);
+		queue.add(marySmith);
+		queue.add(mikeWilson);
+		queue.add(billEnd);
+
+		queue.remove();
+		queue.remove();
+		queue.remove();
+		queue.remove();
+		queue.remove();
+
+		queue.add(mikeWilson);
+
+		queue.printQueue();
+	}
+}
