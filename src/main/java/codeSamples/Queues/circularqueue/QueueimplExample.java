90f4aa6e55e886a3b08c9844f60c7dea510ee207
saurav <sauravpd58@gmail.com>
2018-11-05 19:16:13 +0530

    added QueueimplExample

---
+++ src/main/java/codeSamples/Queues/circularqueue/QueueimplExample.java
@@ -0,0 +1,29 @@
+package codeSamples.Queues.circularqueue;
+
+public class QueueimplExample 
+{
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(5);
+
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
+
+        queue.printQueue();
+
+    }
+}
