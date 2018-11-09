91598ecb5db67c82669d8d5489ef06d6e343e5c1
saurav <sauravpd58@gmail.com>
2018-11-09 20:49:57 +0530

    commit changes in Employee class

--- src/main/java/codeSamples/Stacks/listStack/Employee.java
+++ src/main/java/codeSamples/Stacks/listStack/Employee.java
@@ -1,43 +1,56 @@
 package codeSamples.Stacks.listStack;
 
-public class Employee {
-
+/**
+ * Employee
+ * @author saurav
+ *
+ */
+public class Employee 
+{
     private String firstName;
     private String lastName;
     private int id;
 
-    public Employee(String firstName, String lastName, int id) {
+    public Employee(String firstName, String lastName, int id) 
+    {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
     }
 
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName) 
+    {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
+    public String getLastName() 
+    {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName) 
+    {
         this.lastName = lastName;
     }
 
-    public int getId() {
+    public int getId() 
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id) 
+    {
         this.id = id;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) 
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
@@ -49,7 +62,8 @@ public class Employee {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() 
+    {
         int result = firstName.hashCode();
         result = 31 * result + lastName.hashCode();
         result = 31 * result + id;
@@ -57,12 +71,12 @@ public class Employee {
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Employee{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", id=" + id +
                 '}';
     }
-
 }
